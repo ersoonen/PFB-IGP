bc85512d37eebd9d61185fe61b340ee4ad537eba
Norlidia <s10243253@connect.np.edu.sg>
2023-01-30 13:08:12 +0800

    First commit

---
+++ project_group4/greeter.py
@@ -0,0 +1,4 @@
+## will be used as module
+
+def greet(name):
+    return f"Hello {name}!"
---
+++ project_group4/main.py
@@ -0,0 +1,18 @@
+## this is my main file
+
+## import modules
+import adder, greeter, dbs
+
+## call with module.function()
+
+## call the first function in adder module
+value = adder.add(2, 2)
+print(value) 
+
+double_value = adder.double(value)
+print(double_value)
+
+print(greeter.greet("Mike"))
+
+dbs.dbsacp_function()
+
