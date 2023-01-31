26c9776d5dac1070a7b507f54012851afad0d20d
username <emailaddress>
2023-01-31 17:07:50 +0800

    added file path and reads the csv file

--- project_group4/profit_loss.py
+++ project_group4/profit_loss.py
@@ -1,4 +1,15 @@
-## will be used as module
+## This program will compute the difference in the net profit column if net profit on
+# the current day is lower than the previous day.
 
-def greet(name):
-    return f"Hello {name}!"
+from pathlib import Path
+import csv
+
+fp = Path.cwd()/"project_group4"/"csv_reports"/"profit-and-loss-usd.csv"
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+net_profit = []
+
+for row in reader:
+    if row[4]
