1dc7a9db01d9eae58a1dd3dc63de15f9f4bbd009
username <emailaddress>
2023-01-31 20:33:47 +0800

    changed some codes

--- project_group4/profit_loss.py
+++ project_group4/profit_loss.py
@@ -1,23 +1,38 @@
-## This program will compute the difference in the net profit column if net profit on
-# the current day is lower than the previous day.
+## this will be used as module
 
+#imported Path from pathlib and imported csv
 from pathlib import Path
 import csv
 
+# created fp variable and assigned the file path to the profit-and-loss-usd.csv to it
 fp = Path.cwd()/"project_group4"/"csv_reports"/"profit-and-loss-usd.csv"
+
+# 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
 
-net_profit = []
+net_profit_list = []
 
 for line in reader:
-    net_profit.append(float(line[4]))
+    net_profit_list.append(float(line))
+    net_profit_list.append(float(line[4]))
 
 summary_path = Path.cwd()/"summary_reports.txt"
 
 def net_profit_diff():
     """
-    -
-    -
+    - This function calculates the difference in net profit if net profit on the current
+    day is lower than the previous day
+    - No parameters required.
     """
+    for number in net_profit_list:
+        previous_day = net_profit_list[number][4]
+        current_day = net_profit_list[number+1][4]
+
+        if previous_day > current_day:
+            difference = previous_day - current_day
+            difference_day = net_profit_list[number+1][0]
+
+            with summary_path.open(mode='a', encoding='UTF-8', newline="") as file:
+                file.write(f"[PROFIT DEFICIT] DAY: {difference_day}", f"AMOUNT: USD{difference}" + "\n")
